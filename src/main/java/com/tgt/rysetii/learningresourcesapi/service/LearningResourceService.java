df1ac13cd82222838c2e9ecb00220a9f55c57120
marchingoutlaw <angad.sawadh@gmail.com>
2022-07-14 14:09:21 +0530

    added sorting method

--- src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
@@ -3,20 +3,38 @@ package com.tgt.rysetii.learningresourcesapi.service;
 import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
 import com.tgt.rysetii.learningresourcesapi.entity.LearningResourceStatus;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LearningResourceService {
+    public void saveLearningResource(List<LearningResource> learningResources){
+        saveLearningResourcesToCSV(learningResources);
+    }
     public List<LearningResource> getLearningResource(){
         File learningResourceFile = new File("learningResources.csv");
-        return readLearningResourceFromCSV(learningResourceFile);
+        return readLearningResourcesFromCSV(learningResourceFile);
     }
-    private List<LearningResource> readLearningResourceFromCSV(File csv){
+    public List<Double> getProfitMargins(){
+        List<LearningResource> learningResources = getLearningResource();
+        List<Double> profitMargins = new ArrayList<>();
+        for (LearningResource learningResource:learningResources) {
+            profitMargins.add((learningResource.getSellingPrice()-learningResource.getCostPrice())/learningResource.getSellingPrice());
+        }
+        return profitMargins;
+    }
+    public List<LearningResource> sortByProfitMargin(){
+        List<LearningResource> learningResources = getLearningResource();
+        learningResources.sort((lr1,lr2)->{
+            Double pf1 = (lr1.getSellingPrice()- lr1.getCostPrice())/lr1.getSellingPrice();
+            Double pf2 = (lr2.getSellingPrice()- lr2.getCostPrice())/lr2.getSellingPrice();
+            return pf2.compareTo(pf1);
+        });
+        return learningResources;
+    }
+    private List<LearningResource> readLearningResourcesFromCSV(File csv){
         List<LearningResource> learningResources = new ArrayList<>();
         try {
             FileReader fr1 = new FileReader(csv);
@@ -47,5 +65,35 @@ public class LearningResourceService {
         LocalDate retiredDate = LocalDate.parse(attributes[7],dtf1);
         LearningResource learningResource = new LearningResource(id, name, costPrice, sellingPrice, learningResourcestatus, createdDate, publishedDate, retiredDate);
         return learningResource;
+    }
+
+    private void saveLearningResourcesToCSV(List<LearningResource> learningResources){
+        try{
+            File learningResourceFile = new File("learningResources.csv");
+            FileWriter fw1 = new FileWriter(learningResourceFile, true);
+            BufferedWriter bw1 = new BufferedWriter(fw1);
+            for (LearningResource learningResource : learningResources) {
+                bw1.newLine();
+                String data = "";
+                data.concat(learningResource.getId().toString());
+                data.concat(",");
+                data.concat(learningResource.getName());
+                data.concat(",");
+                data.concat(learningResource.getCostPrice().toString());
+                data.concat(",");
+                data.concat(learningResource.getSellingPrice().toString());
+                data.concat(",");
+                data.concat(learningResource.isProductStatus().toString());
+                data.concat(",");
+                data.concat(learningResource.getCreatedDate().toString());
+                data.concat(",");
+                data.concat(learningResource.getPublishedDate().toString());
+                data.concat(",");
+                data.concat(learningResource.getRetiredDate().toString());
+                bw1.write(data);
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
